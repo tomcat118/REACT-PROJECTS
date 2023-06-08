f0592980398cbd8d65a148239218fe3862e0d7bf
kyzhao188 <zky19990705@icloud.com>
2023-06-08 15:10:18 -0400

    top-charts and top-artists cards

--- Music_app/src/components/TopPlay.jsx
+++ Music_app/src/components/TopPlay.jsx
@@ -10,6 +10,18 @@ import { useGetTopChartsQuery } from '../redux/services/shazamCore';
 
 import 'swiper/css';
 import 'swiper/css/free-mode';
+ 
+{/* TopChart cards that display one song from most popular song list */}
+const TopChartCard = ( {song, i}) => (
+  <div className ="w-full flex flex-row items-center hover:bg-[#4c426e] py-2 p-4 rounded-lg cursor-pointer mb-2">
+    
+    <h3 className = "fot-bold text-base text-white mr-3"> { i+ 1 }</h3>
+    <div className = "flex-1 flex flex-row justify-between items-center">
+      <img className = "w-20 h-20 rounded-lg" src ={song?.images?.coverart} alt = {song?.title} 
+      />
+    </div>
+  </div>
+)
 {/* topcharts that list popular songs and able to play*/}
 const TopPlay = () => {
   {/* Initialize states including dispatch; activeSong, isPlaying from player, data*/}
@@ -41,7 +53,45 @@ const TopPlay = () => {
             </Link>
           </div>
 
-
+          <div className = "mt-4 flex flex-col gap-1">
+            {topPlays?.map((song, i) =>(
+              <TopChartCard 
+                key = {song.key}
+                song = {song}
+                i ={i}
+              />
+            ))}
+          </div>
+        </div>
+          
+        <div className = "w-full flex flex-col mt-8">
+        <div className = "flex flex-row justify-between items-center">
+            <h2 className = "text-white font-bold text-2xl"> Top Artists</h2>
+            <Link to = "/top-artists">
+              <p className = "text-gray-300 text-base cursor-pointer">See more</p>
+            </Link>
+          </div>
+          <Swiper
+            slidesPerView = "auto"
+            spaceBetween = {15}
+            freeMode
+            centeredSlides
+            centeredSlidesBounds
+            modules = {[FreeMode]}
+            className = "mt-4"
+          >
+            {topPlays?.map((song, i) => (
+              <SwiperSlide
+                key ={song.key}
+                style = {{ width: `25%` , height: `auto`}}
+                className = "shadow-lg rounded-full animate-slideright"
+              >
+                <Link to = {`/artists/${song?.artists[0].adamid}`}>
+                  <img src = {song?.images.background} alt = "name" className = "rounded-full w-full object-cover" />
+                </Link>
+              </SwiperSlide>
+            ))}
+          </Swiper>
         </div>
     </div>
 
