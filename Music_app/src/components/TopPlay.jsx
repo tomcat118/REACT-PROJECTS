ac9e989f7441129ac468956ae94794ece8d04b77
kyzhao188 <zky19990705@icloud.com>
2023-06-08 15:20:35 -0400

    playing song for top charts

--- Music_app/src/components/TopPlay.jsx
+++ Music_app/src/components/TopPlay.jsx
@@ -12,14 +12,32 @@ import 'swiper/css';
 import 'swiper/css/free-mode';
  
 {/* TopChart cards that display one song from most popular song list */}
-const TopChartCard = ( {song, i}) => (
+const TopChartCard = ( {song, i , isPlaying, activeSong, handlePlayClick, handlePauseClick}) => (
   <div className ="w-full flex flex-row items-center hover:bg-[#4c426e] py-2 p-4 rounded-lg cursor-pointer mb-2">
-    
     <h3 className = "fot-bold text-base text-white mr-3"> { i+ 1 }</h3>
     <div className = "flex-1 flex flex-row justify-between items-center">
       <img className = "w-20 h-20 rounded-lg" src ={song?.images?.coverart} alt = {song?.title} 
-      />
+      /> 
+      <div className = "flex-1 flex flex-col justify-center mx-3">
+        <Link to = {`/songs/${song.key}`}>
+          <p className = "text-xl font-bold text-white">
+            {song?.title}
+          </p>
+        </Link>
+        <Link to = {`/artists/${song?.artists[0].adamid}`}>
+          <p className = "text-ase text-gray-300 mt-1">
+            {song?.subtitle}
+          </p>
+        </Link>
+      </div>
     </div>
+    <PlayPause 
+      isPlaying = {isPlaying}
+      activeSong = {activeSong}
+      song = {song}
+      handlePause = {handlePauseClick}
+      handlePlay = {handlePlayClick}
+    />
   </div>
 )
 {/* topcharts that list popular songs and able to play*/}
@@ -39,7 +57,7 @@ const TopPlay = () => {
   const handlePauseClick = () => {
     dispatch(playPause(false));
   };
-  const handlePlayClick = () => {
+  const handlePlayClick = (song, i) => {
     dispatch(setActiveSong({ song, data, i }));
     dispatch(playPause(true));
   };
@@ -59,6 +77,10 @@ const TopPlay = () => {
                 key = {song.key}
                 song = {song}
                 i ={i}
+                isPlaying = {isPlaying}
+                activeSong = { activeSong }
+                handlePauseClick = {handlePauseClick}
+                handlePlayClick = {() => handlePlayClick(song,i)}
               />
             ))}
           </div>
