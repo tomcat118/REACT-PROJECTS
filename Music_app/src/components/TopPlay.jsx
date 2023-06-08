32e48b48148746b014da31c4e819dc2b631a66f2
kyzhao188 <zky19990705@icloud.com>
2023-06-08 14:44:43 -0400

    add a see more link to top charts on page

--- Music_app/src/components/TopPlay.jsx
+++ Music_app/src/components/TopPlay.jsx
@@ -18,7 +18,7 @@ const TopPlay = () => {
   const { data } = useGetTopChartsQuery();
   const divRef = useRef(null);
   
-  {/* apply scroll to top effect to the topplay page */}
+  {/* apply scroll to top effect to the page */}
   useEffect(( ) => {
     divRef.current.scrollIntoView({ behavior: 'smooth'});
   })
@@ -32,9 +32,19 @@ const TopPlay = () => {
     dispatch(playPause(true));
   };
   return (
-    <div ref ={divRef}>
+    <div ref ={divRef} className = "xl:ml-6 ml-0 xl:mb-0 mb:6 flex-1 xl:max-w-[500px] max-w-full flex flex-col">
+        <div className = "w-full flex flex-col">
+          <div className = "flex flex-row justify-between items-center">
+            <h2 className = "text-white font-bold text-2xl"> Top Charts</h2>
+            <Link to = "/top-charts">
+              <p className = "text-gray-300 text-base cursor-pointer">See more</p>
+            </Link>
+          </div>
 
+
+        </div>
     </div>
+
   )
 };
 
